77b061eb9706d5322ca8abf3c684c79f92c4e0a5
mernahanna23 <158622609+mernahanna23@users.noreply.github.com>
2024-02-17 14:58:59 +0200

    Add files via upload

--- so.php
+++ so.php
@@ -1,1 +1,20 @@
+<?php
+// data source name have inside prefex of my sql
+$dsn='mysql:host=localhost;dbname=library_db';
+//user to connect
+$user='root';
+//password to this user
+$pass='';
+try {
 
+$db= new PDO($dsn,$user,$pass); // start a new connection with pdo class
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$q="INSERT INTO author ( first_name, last_name, age) VALUES ( 'chales', 'holmes', '40')";
+$db->exec($q);
+}
+
+catch(PDOException $e){
+    echo 'fail' . $e->getMessage();
+}
+?>
